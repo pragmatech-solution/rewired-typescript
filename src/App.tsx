102ff1065677fb860202cad7638e2448de1f51e0
Pragmatic <jhon.tesfamariam21@gmail.com>
2022-08-31 02:05:15 +0300

    configured with module.less

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.less";
 import { Button } from "antd";
+import AppHeader from "./AppHeader";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button type="primary">Click me</Button>
+        <AppHeader />
       </header>
     </div>
   );
---
+++ src/AppHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AppHeader.module.less";
+
+function AppHeader() {
+  return <h1 className={styles.header}>Head</h1>;
+}
+
+export default AppHeader;
